9f4382b665108a2d18b95b3e56ffad16f4ac046a
Jesse Alltop <Jesse@macbook-pro-2.local>
2016-09-15 15:46:53 -0500

    getting there

--- workoutFinal/ViewController.swift
+++ workoutFinal/ViewController.swift
@@ -68,7 +68,7 @@ class ViewController: UIViewController, URLSessionDataDelegate, UITextFieldDeleg
 //        let requestURL: NSURL = NSURL(string: "http://workoutapptesting.ddns.net/getusers.php")!
 //      let requestURL: NSURL = NSURL(string: "http://localhost/getusers.php")!
       let requestURL: NSURL = NSURL(string: "http://98.253.68.160/getusers.php")!
-        
+        var userNamesArray = [String]()
         let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
         let session = URLSession.shared()
         let task = session.dataTask(with: urlRequest as URLRequest) {
@@ -79,44 +79,41 @@ class ViewController: UIViewController, URLSessionDataDelegate, UITextFieldDeleg
             
             if (statusCode == 200) {
                 
-                var jsonResult: NSDictionary = NSDictionary()
-               
+                //var jsonResult: AnyObject
+                
                 do{
                     
-                    jsonResult = try JSONSerialization.jsonObject(with: data!) as! NSDictionary
+                   // jsonResult = try JSONSerialization.jsonObject(with: data!) as! NSDictionary
+                    
+                    var jsonResult = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
+                    
+                    
+                    
+                    if let users = jsonResult["USER"] as? [String: AnyObject] {
+                        if let userNames = users["USERNAME"] {
+
+                            for i in 0..<userNames.count{
+                                let usrnm = userNames[i] as! String
+                                print(usrnm)
+                                userNamesArray.append(usrnm)
+                                
+                            }
+                        
+                        
+                        }
+                    }
+                    
+                    
                         
                     print(jsonResult)
 
-                    
                 
-                    var userObjList: NSMutableArray
-                    userObjList = jsonResult.mutableArrayValue(forKey: "USER")
                     
-//                    let fnameList = userObjList.mutableArrayValue(forKey: "FNAME")
-//                    let lnameList = userObjList.mutableArrayValue(forKey: "LNAME")
-//                    let emailList = userObjList.mutableArrayValue(forKey: "EMAIL")
-//                    let usernameList = userObjList.mutableArrayValue(forKey: "USERNAME")
-//                    let passwordList = userObjList.mutableArrayValue(forKey: "PASSWORD")
-//                    let weightList = userObjList.mutableArrayValue(forKey: "WEIGHT")
-//                    let heightList = userObjList.mutableArrayValue(forKey: "HEIGHT")
-//                    var userHelper = userObj()
+                    
                     
 
-//                    print(fnameList.count)
                     
-                    
-                    
-//                    for i in (0..<userObjList.count) {
-//                        print(i)
-//
-//                        userHelper =  userObj(FNAME: fnameList[i] as! String,LNAME: lnameList[i] as! String,EMAIL: emailList[i] as! String,USERNAME: usernameList[i] as! String,PASSWORD: passwordList[i] as! String,WEIGHT: weightList[i] as! Int,HEIGHT: heightList[i] as! Int)
-//                        
-//                        self.primaryUserArray.add(userHelper)
-//                        
-//                        
-//                        }
-                    
-                    
+
        
                     } catch let error as NSError {
                         print(error)
@@ -134,38 +131,4 @@ class ViewController: UIViewController, URLSessionDataDelegate, UITextFieldDeleg
     }
     
     
-    
-    private static func usersFromJsonObject(json: [String: AnyObject]) -> userObj? {
-        guard let
-            firstName = json["FNAME"] as? String,
-            lastName = json["LNAME"] as? String,
-            email = json["EMAIL"] as? String,
-            username = json["USERNAME"] as? String,
-            password = json["PASSWORD"] as? String,
-            weight = json["WEIGHT"] as? Int,
-            height = json["HEIGHT"] as? Int else{
-                
-                //Don't have enough information ti construct a user
-                return nil
-        }
-        return userObj(FNAME: firstName, LNAME: lastName, EMAIL: email, USERNAME: username, PASSWORD: password, WEIGHT: weight, HEIGHT: height)
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
-
-    
-    
-    
-    
-
